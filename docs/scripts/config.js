fa94449490b1bafe733004bc292627f70374f0be
Sravanthi Talluri <s.19291@gmail.com>
2023-05-30 18:14:02 +0530

    Changes for Chat Translator

---
+++ docs/scripts/config.js
@@ -0,0 +1,14 @@
+export default {
+    // Token Implicit Grant Client ID
+    clientID: '360513bd-fe43-46e7-bb79-b7fb3ae38f62',
+
+    redirectUri: 'https://localhost/',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        region: 'usw2.pure.cloud'
+    },
+
+    translateServiceURI: 'https://api-genesys.azurewebsites.net/translate'
+}
